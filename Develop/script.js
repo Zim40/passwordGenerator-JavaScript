d5c6082ed60352ce538818f0968d377bed1d3724
User <michaelm810129@gmail.com>
2022-12-17 13:55:17 +1030

    created variables/created length function/created upper and lowercase prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,47 @@
 // Assignment code here
 
+// Variables
+ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+ var Numerical = "0123456789";
+ var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+ var passWord = "";
+console.log(upperCase + lowerCase + Numerical + specialChars.length);
+// Prompt for password length
+function generatePassword () {
+
+  var length = window.prompt("Choose a Password length between 8 and 128 characters long.")
+    console.log(length);
+  if (length < 8 || length > 128) {
+    alert("Your password must be between 8 and 128 characters!");
+  } 
+  return(generatePassword);
+
+}
+
+var upCase = window.confirm("Would you like Upper case letters?");
+console.log(upCase);
+if (upCase == [true]){
+  for (var i = 0; i < upperCase.length; i++)
+  passWord.push(upperCase[i]);
+}
+var lowCase = window.confirm("Would you like lowers case letters?");
+console.log(lowCase);
+if(lowCase == [true]) {
+  for (var i = 0; i <lowerCase.length; i++)
+  passWord.push(lowerCase[i]);
+}
+var Numbers = window.prompt("would you like to add numbers");
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
