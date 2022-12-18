9ff2a84e5c8d3d7c18fdfba55382c9ef1eb7ce0e
User <michaelm810129@gmail.com>
2022-12-18 13:45:03 +1030

    adding changes for generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,71 @@
 // Assignment code here
 
 // Variables
-var generatepassword = "password";
+
  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
  var Numerical = "0123456789";
  var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
- var password = "";
+ var password = [];
 console.log(upperCase.length + lowerCase.length + Numerical.length + specialChars.length);
 
 
 // Prompt for password length
 function generatePassword () {
-
   var length = window.prompt("Choose a Password length between 8 and 128 characters long.")
-    console.log(length);
-  if (length < 8 || length > 128) {
-    alert("Your password must be between 8 and 128 characters!");
-  } 
-  return(generatePassword);
+  console.log(length);
+if (length < 8 || length > 128) {
+  alert("Your password must be between 8 and 128 characters!");
+  return password;
+} 
+
 
 }
-  // Add Uppercase letters
-  var upCase = window.confirm("Would you like Upper case letters?");
-  console.log(upCase);
-  if (upCase == [true]){
-    for (var i = 0; i < upperCase.length; i++)
-    password.push(upperCase[i]);
-  }
-  // Add lower case letters
-  var lowCase = window.confirm("Would you like lowers case letters?");
-  console.log(lowCase);
-  if(lowCase == [true]) {
-    for (var i = 0; i <lowerCase.length; i++)
-    password.push(lowerCase[i]);
-  }
-  // Add numbers 
-  var Numbers = window.confirm("would you like to add numbers");
-  console.log(Numbers);
-  if (Numbers == [true]) {
-    for (var i = 0; i <Numerical.length; i++)
-    password.push(Numerical[i]);
-  }
-  // Add special characters 
-  var specChar = window.confirm("Would you like special characters");
-  console.log(specChar);
-  if (specChar == [true]) {
-    for (var i = 0; i <specialChars.length; i++)
-    password.push(specialChars[i]);
-  }
-  for (var i = 0; i< length; i++){
-password = password + [Math.floor(Math.random() * length)];
-  }
+// Add Uppercase letters
+var upCase = window.confirm("Would you like Upper case letters?");
+console.log(upCase);
+if (upCase == [true]){
+  for (var i = 0; i < upperCase.length; i++)
+  password.push(upperCase[i]);
+  
+}
+// Add lower case letters
+var lowCase = window.confirm("Would you like lowers case letters?");
+console.log(lowCase);
+if(lowCase == [true]) {
+  for (var i = 0; i <lowerCase.length; i++)
+  password.push(lowerCase[i]);
+  
+}
+// Add numbers 
+var Numbers = window.confirm("would you like to add numbers");
+console.log(Numbers);
+if (Numbers == [true]) {
+  for (var i = 0; i <Numerical.length; i++)
+  password.push(Numerical[i]);
+  
+}
+// Add special characters 
+var specChar = window.confirm("Would you like special characters");
+console.log(specChar);
+if (specChar == [true]) {
+  for (var i = 0; i < specialChars.length; i++)
+  password.push(specialChars[i]);
+  
+}
+// Generating Password
+for (var i = 0; i< length; i++){
+  password = password + [Math.floor(Math.random() * length)];
+  console.log(password);
+  return password;
 
 
+
+
+
+   
+    
+  }
 
 
 
@@ -68,6 +80,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
